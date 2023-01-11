a578e246a8f30368af3fac156ca78513e0fdbfe3
Viktor Reichert <viktor.reichert@conteco.gmbh>
2023-01-11 13:13:08 +0100

    Initial Commit

---
+++ src/main/java/gmbh/conteco/spring_schulung_jpa/SpringSchulungJpaApplication.java
@@ -0,0 +1,13 @@
+package gmbh.conteco.spring_schulung_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchulungJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSchulungJpaApplication.class, args);
+    }
+
+}
